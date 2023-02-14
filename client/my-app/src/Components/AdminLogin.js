26326ef0322e4b889a31db58ccba99807fa9d54a
Parth Soni <82641.parthsoni@gmail.com>
2023-02-14 11:28:54 +0530

    customer data in

--- client/my-app/src/Components/AdminLogin.js
+++ client/my-app/src/Components/AdminLogin.js
@@ -1,20 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Form.css"
+import Axios from "axios";
 
 export default function AdminLogin() {
+  const[username, changeUsername]=useState("");
+  const[password,changePassword]=useState("");
+
+  const admin_login=()=>{
+    console.log("admin login")
+    Axios.post("http://localhost:3001/admin_login",{
+      username: username,
+      password: password
+    }).then((response)=>{
+      console.log(response);
+    })
+  }
+
   return (
     <>
         <main>
             <h2>Admin login</h2>
             <div className="input-group">
                 Enter your username
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" onChange={(e)=>{changeUsername(e.target.value);}}/>
             </div>
             <div className="input-group">
                 Enter your password
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" onChange={(e)=>{changePassword(e.target.value);}}/>
             </div>
-            <button>Submit</button>
+            <button onClick={admin_login}>Submit</button>
 
         </main>
     </>
--- server/index.js
+++ server/index.js
@@ -25,12 +25,21 @@ conn.connect(function(err)
     if(err) throw err;
     console.log("Connected!");
 })
+
+// ***********table farmer ************
 // conn.query("create table farmer(fname varchar(15), lname varchar(15),age int, aadhar_no varchar(15), unique_id int, phone_no varchar(15), city varchar(15), state varchar(15), pincode int, password int)", function(err,result)
 // {
 //     if(err) throw err;
 //     console.log("result"+result);
 // })
 
+
+//  *** table customer ******
+// conn.query("create table customer(fname varchar(20), lname varchar(20), phone_no varchar(15), email varchar(15), address varchar(15), state varchar(15), city varchar(20), password varchar(20));",function(err,Result)
+// {
+//     if(err)throw err;
+//     console.log(Result);
+// })
 
 // app.use(function(req,res,next){
 //     console.log("Middleware called");
@@ -47,8 +56,18 @@ app.post("/farmer_login",function(req,res){
 })
 app.post("/customer_login",function(req,res){
     console.log(req.body);
+    conn.query("insert into customer(fname, lname, phone_no,email, address, state, city, password) values('"+req.body.fname+"','"+req.body.lname+"','"+req.body.phone_no+"','"+req.body.email+"','"+req.body.address+"','"+req.body.state+"','"+req.body.city+"','"+req.body.password+"');",function(err,result){
+        if(err)throw err;
+        console.log(result);
+    })
     res.send("we did it");
+
 })
+app.post("/admin_login",function(req,res){
+    console.log(req.body);
+    res.send("admin here");
+})
+
 app.get("/",function(req,res){
     console.log("user called us");
     res.send("hello");

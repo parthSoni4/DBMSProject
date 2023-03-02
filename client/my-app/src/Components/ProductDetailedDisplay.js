6e6a5c030bf1e8abeed79d4a6268031d34ed32b6
Parth Soni <82641.parthsoni@gmail.com>
2023-03-02 15:57:10 +0530

    product detailed display

--- client/my-app/src/Components/ProductDetailedDisplay.js
+++ client/my-app/src/Components/ProductDetailedDisplay.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import  {useLocation} from "react-router-dom";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
 
 export default function ProductDetailedDisplay() {
     // console.log(product_id);
     const location=useLocation();
+    const [data, setData]=useState([]);
     const id=location.state.a;
     
     useEffect(() => {
-      axios.post("https://localhost:3001/product_detailed_display", {id})
-      .then(response=>console.log(response.data))
+      axios.post("http://localhost:3001/product_detailed_display", {id})
+      .then(response=>{console.log(response.data)
+        setData(response.data)
+        console.log(response.data.id);
+        })
       .catch(error=>console.log(error));
     }, [])
     
@@ -18,7 +22,9 @@ export default function ProductDetailedDisplay() {
   return (
     <>
     <div>ProductDetailedDisplay</div>
-    <div>{id}</div>
+    {/* <div>{id}</div>
+     */}
+     <h1>{data.id}</h1>
     </>
   )
 }

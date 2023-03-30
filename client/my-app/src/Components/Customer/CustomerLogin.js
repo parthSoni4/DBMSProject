08f337699e6112a842aa8b18b8c3f4ddde6e5807
Dishanth P <dishanthprabhakar206@gmail.com>
2023-03-30 21:47:59 +0530

    Added show password code

--- client/my-app/src/Components/Customer/CustomerLogin.js
+++ client/my-app/src/Components/Customer/CustomerLogin.js
@@ -1,59 +1,70 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "../Form.css";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 export default function CustomerLogin() {
-    const navigate=useNavigate("/root");
+    const navigate = useNavigate("/root");
 
-    const[name,changeName]=useState("");
-    const[password,changePassword]=useState("");
-    const[status,changeStatus]=useState("");
+    const [name, changeName] = useState("");
+    const [password, changePassword] = useState("");
+    const [status, changeStatus] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
-    const check_customer=()=>{
-        axios.post("http://localhost:3001/check_customer",{
+
+    const check_customer = () => {
+        axios.post("http://localhost:3001/check_customer", {
             name: name,
             password: password
-        }).then((response)=>{
+        }).then((response) => {
             // console.log(response);
-            if(response.data==="wrong")
-            {
+            if (response.data === "wrong") {
                 changeStatus("Wrong username or password");
             }
-            else
-            {
+            else {
                 changeStatus("Welcome");
                 console.log(response.data[0].fname);
-                const customer_id=response.data[0].customer_id;
+                const customer_id = response.data[0].customer_id;
                 navigate(`../CustomerOption/customer_id/${customer_id}`);
-                sessionStorage.setItem("customer_id",customer_id);
+                sessionStorage.setItem("customer_id", customer_id);
             }
         })
     };
 
     useEffect(() => {
-      document.body.style.backgroundColor="pink";
-    //   document.body.style.backgroundColor="red";
+        document.body.style.backgroundColor = "pink";
+        //   document.body.style.backgroundColor="red";
     }, [])
-    
 
-  return (
-    <main>
-        <h2>Customer Login </h2>
-        <div className="input-group">
-            <input type="text" placeholder="Enter your name ..." className="form-control" onChange={(e)=>{changeName(e.target.value);}}/>
-        </div>
-        <div className="input-group">
-            <input type="text" placeholder="Enter your password ..." className="form-control" onChange={(e)=>{changePassword(e.target.value);}} />
-        </div>
-        <div>
-        {/* <a href class="login_a" >Forgot password?</a> */}
-        </div>
-        <div className="input-group">
-            <button className="btn btn-default btn-primary option-button" onClick={check_customer}>Submit</button>
-        </div>
-        <h3>{status}</h3>
-    </main>
-  )
+    const handleToggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <main>
+            <h2>Customer Login </h2>
+            <div className="input-group">
+                <input type="text" placeholder="Enter your name ..." className="form-control" onChange={(e) => { changeName(e.target.value); }} />
+            </div>
+            <div className="input-group">
+                <input type={showPassword ? 'text' : 'password'} placeholder="Enter your password ..." className="form-control" onChange={(e) => { changePassword(e.target.value); }} />
+            </div>
+            <div className="input-group">
+                <input
+                    type="checkbox"
+                    checked={showPassword}
+                    onChange={handleToggleShowPassword}
+                />
+                Show password
+            </div>
+            <span>
+                <a href class="login_a" >Forgot password?</a>
+            </span>
+            <div className="input-group">
+                <button className="btn btn-default btn-primary option-button" onClick={check_customer}>Submit</button>
+            </div>
+            <h3>{status}</h3>
+        </main>
+    )
 }

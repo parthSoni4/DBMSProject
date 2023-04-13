148d7aadce2a4652745797f54ffe66e706ba9d7e
Parth Soni <82641.parthsoni@gmail.com>
2023-04-13 18:21:28 +0530

    count 7

--- client/my-app/src/Components/About.js
+++ client/my-app/src/Components/About.js
@@ -5,26 +5,29 @@ import { useEffect } from 'react';
 import image from "./HomeImages/farmer1.jpg";
 
 export default function About() {
-    useEffect(() => {
-        const style = document.createElement('style');
-        style.textContent = `body { background-image: url(https://soygrowers.com/wp-content/uploads/2019/02/hompage-hero-img.jpg); }`;
-        document.head.appendChild(style);
+    // useEffect(() => {
+    //     const style = document.createElement('style');
+    //     style.textContent = `body { background-image: url(https://soygrowers.com/wp-content/uploads/2019/02/hompage-hero-img.jpg); }`;
+    //     document.head.appendChild(style);
     
-      }, )
+    //   }, )
     return (
         <>  {/* fragment tag */}
-            <main>
+            <main className='main-about'>
+                <section className="heading-about">
+                    <h2> About us</h2>
+                </section>
                 <div className="container about-container">
                     {/* <h2>About us</h2> */}
                     <div className="row row-about">
-                        <div className="col-sm">
+                        {/* <div className="col-sm">
                              <img src={farmer1} alt="" className='img-responsive about-img'/>
-                        </div>
+                        </div> */}
                         <div className="col-sm">
-                            <h2>
+                            <h2 className='text-center'>
                                 Friends for our farmer
                             </h2>
-                            <p>
+                            <p className='text-center about-text'>
                                 This website is made for our beloved farmer. This is a small try to help them financially and increase their profit percentage. This website aim to bridge the gap between the customer and farmer. We want customer to connect with the farmer and know the importance of agriculture in today's wortld. 
                             </p>
                         </div>

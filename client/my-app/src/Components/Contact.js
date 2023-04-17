909570e7905aa02fd8a3f2e3b6f72a767a563ad3
Dishanth P <dishanthprabhakar206@gmail.com>
2023-04-18 00:53:35 +0530

    Feedback form updated

--- client/my-app/src/Components/Contact.js
+++ client/my-app/src/Components/Contact.js
@@ -3,79 +3,111 @@ import "./Form.css";
 import axios from "axios";
 import image from "./Farmer/FarmerSectionImage/wow.jpg";
 import image2 from "./HomeImages/lets.jpg";
+import swal from "sweetalert";
 
 export default function Contact() {
-    const[name,setName]=useState("");
-    const[email,setEmail]=useState("");
-    const[description,setDescription]=useState("");
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        description: ""
+    });
+
     const [errors, setErrors] = useState({});
 
-    const Submit=()=>{
-        console.log(name);
-        console.log(email);
-        console.log(description);
+    const handleChange = (e) => {
+        setFormData({...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errors = {};
+
+        if (!formData.name.trim()) {
+            errors.name = "Name is required";
+        } else if (!/^[A-Za-z]+(?:\s[A-Za-z]+)*$/.test(formData.name)) {
+            errors.name = "Name must be characters only";
+        }
+
+        if (!formData.email.trim()) {
+            errors.email = "Email is required";
+          } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+            errors.email = "Invalid email format";
+          }
+
+          if (!formData.description.trim()) {
+            errors.description = "Description is required";
+          } else if (formData.description.length > 60) {
+            errors.description = "Description must be less than 100 characters";
+          }
+      
+
+        if (Object.keys(errors).length > 0) {
+            setErrors(errors);
+        } else {
+            console.log("Feedback submitted successfully");
+            swal({title: "FeedBack submitted !", text: "You will get the reply through mail", icon: "success", button: "OK"});
+            
         axios.post("http://localhost:3001/contact",{
-            name: name,
-            email: email,
-            description: description
+            
+            name: formData.name,
+            email: formData.email,
+            description: formData.description
         }).then((response)=>{
             console.log(response);
         });
+        setFormData({
+            name: "",
+            email: "",
+            description: ""
+        });
+        setErrors({});
     }
-    // useEffect(() => {
-    //         const style = document.createElement('style');
-    //         style.textContent = `body { background-image: url(${image2}); background-size: cover; background-repeat: no-repeat; }`;
-    //         document.head.appendChild(style);
-        
-    //       }, )
-
-    useEffect(() => {
-        validateForm();
-       },)
-
-       const validateForm = () => {
-        const errors = {};
-        if (!name) {
-          errors.name = "*";
-        } else if (!/^[A-Za-z]+(?:\s[A-Za-z]+)*$/.test(name)) {
-          errors.name = "Name must be characters only";
-        }
-         
-        if (!email.trim()) {
-            errors.email = "*";
-        } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-            errors.email = "\nEnter the valid email address";
-         }
-
-        if (!description.trim()) {
-            errors.description = "*";
-        } 
-      
-        setErrors(errors);
-      };
+};
+const terms = (e) => {
+    e.preventDefault();
     
+};
+   
+    
+      
       const errorStyle = { color: "red" };
     
   return (
     <>
-    <form onSubmit={validateForm}>
+    <form onSubmit={handleSubmit}>
         <h2>Message and feedback form</h2>
         <div className="input-group">
-            <input type="text" placeholder='Enter your name ...' className="form-control" name="name" onChange={(e)=>{setName(e.target.value);}}/>
-            {errors.name && <span style={errorStyle}>{errors.name}</span>}        
+            <input type="text" placeholder='Enter your name ...' className="form-control" id="name" name="name"  
+            onChange={handleChange}/> {
+                errors.name && <span style={errorStyle}>
+                    {
+                    errors.name
+                }</span>}
+                   
         </div>
         <div className="input-group">
-            <input type="text" placeholder="Enter your email ..." className="form-control" name="email" onChange={(e)=>{setEmail(e.target.value);}} />
-            {errors.email && <span style={errorStyle}>{errors.email}</span>}
-        </div>
+            <input type="text" placeholder="Enter your email ..." className="form-control" name="email" 
+            onChange={handleChange}/> {
+                errors.email && <span style={errorStyle}>
+                    {
+                    errors.email
+                }</span>}
+           </div>
         <div className="input-group1">
-            {errors.description && <span style={errorStyle}>{errors.description}</span>}
-            <textarea placeholder='Enter your message and description ...' name="description" cols="30" rows="10" className="form-control" onChange={(e)=>{setDescription(e.target.value);}} />
+            <textarea placeholder='Enter your message and description ...  (must be less than 100 characters)' name="description" cols="150" rows="10" className="form-control" 
+           onChange={handleChange}/> {
+            errors.description && <span style={errorStyle}>
+                {
+                errors.description
+            }</span>}
         </div>
         <div className="input-group">
-            <button className="btn btn-primary option-button" onClick={Submit}>Submit</button>
+            <button className="btn btn-primary option-button">Submit</button>
         </div>
     </form>
     </>
   )
 }
+
